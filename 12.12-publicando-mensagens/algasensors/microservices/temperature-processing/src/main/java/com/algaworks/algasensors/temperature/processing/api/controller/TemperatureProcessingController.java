688a13e68392c8379bfb2e381315a27bd1f56445
Alex Augusto <alexaugustoti@gmail.com>
2025-04-03 15:33:25 -0300

    novas aulas

---
+++ 12.12-publicando-mensagens/algasensors/microservices/temperature-processing/src/main/java/com/algaworks/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
@@ -0,0 +1,57 @@
+package com.algaworks.algasensors.temperature.processing.api.controller;
+
+import com.algaworks.algasensors.temperature.processing.api.model.TemperatureLogOutput;
+import com.algaworks.algasensors.temperature.processing.common.IdGenerator;
+import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.OffsetDateTime;
+
+import static com.algaworks.algasensors.temperature.processing.infrastructure.rabbitmq.RabbitMQConfig.FANOUT_EXCHANGE_NAME;
+
+@RestController
+@RequestMapping("/api/sensors/{sensorId}/temperatures/data")
+@Slf4j
+@RequiredArgsConstructor
+public class TemperatureProcessingController {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
+    public void data(@PathVariable TSID sensorId, @RequestBody String input) {
+        if (input == null || input.isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        Double temperature;
+
+        try {
+            temperature = Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        TemperatureLogOutput logOutput = TemperatureLogOutput.builder()
+                .id(IdGenerator.generateTimeBasedUUID())
+                .sensorId(sensorId)
+                .value(temperature)
+                .registeredAt(OffsetDateTime.now())
+                .build();
+
+        log.info(logOutput.toString());
+
+        String exchange = FANOUT_EXCHANGE_NAME;
+        String routingKey = "";
+        Object payload = logOutput;
+
+        rabbitTemplate.convertAndSend(exchange, routingKey, payload);
+
+    }
+
+}
---
+++ 12.16-competing-consumers-pattern/algasensors/microservices/temperature-monitoring/src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
@@ -0,0 +1,33 @@
+package com.algaworks.algasensors.temperature.monitoring.infrastructure.rabbitmq;
+
+import com.algaworks.algasensors.temperature.monitoring.api.model.TemperatureLogData;
+import com.algaworks.algasensors.temperature.monitoring.domain.service.TemperatureMonitoringService;
+import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.Map;
+
+import static com.algaworks.algasensors.temperature.monitoring.infrastructure.rabbitmq.RabbitMQConfig.QUEUE;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RabbitMQListener {
+
+    private final TemperatureMonitoringService temperatureMonitoringService;
+
+    @RabbitListener(queues = QUEUE, concurrency = "2-3")
+    @SneakyThrows
+    public void handle(@Payload TemperatureLogData temperatureLogData) {
+        temperatureMonitoringService.processTemperatureReading(temperatureLogData);
+        Thread.sleep(Duration.ofSeconds(5));
+    }
+
+}
