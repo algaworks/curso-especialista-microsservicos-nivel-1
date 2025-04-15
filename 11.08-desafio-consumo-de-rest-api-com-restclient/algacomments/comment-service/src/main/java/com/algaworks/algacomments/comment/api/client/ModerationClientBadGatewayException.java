78246ccb38219abcfaa9a458f8f5dac863336b1d
Alex Augusto <alexaugustoti@gmail.com>
2025-04-15 11:56:33 -0300

    correcao aulas

--- 11.08-desafio-consumo-de-rest-api-com-restclient/algacomments/comment-service/src/main/java/com/algaworks/algacomments/comment/api/client/ModerationClientBadGatewayException.java
+++
@@ -1,8 +0,0 @@
-package com.algaworks.algacomments.comment.api.client;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_GATEWAY)
-public class ModerationClientBadGatewayException extends RuntimeException {
-}
--- 11.08-desafio-consumo-de-rest-api-com-restclient/algacomments/comment-service/src/main/java/com/algaworks/algacomments/comment/domain/exception/ModerationRejectedException.java
+++
@@ -1,7 +0,0 @@
-package com.algaworks.algacomments.comment.domain.exception;
-
-public class ModerationRejectedException extends RuntimeException {
-    public ModerationRejectedException(String message) {
-        super(message);
-    }
-}
